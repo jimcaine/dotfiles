b6bdf41eaa22d666835e261f9a8edbf1ed333b4c
Jim Caine <caine.jim@gmail.com>
2023-10-24 08:46:01 -0500

    setup-scripts: add wallpaper download script

---
+++ setup-scripts/wallpapers.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.wallpapers
+cd ~/.wallpapers
+wget -A jpg https://github.com/makccr/wallpapers/raw/master/wallpapers/animals/anna-wangler-_GqwoiT7QY8-unsplash.jpg
+wget -A jpg https://images.unsplash.com/photo-1534870439272-475575042b61\?ixlib\=rb-4.0.3\&q\=85\&fm\=jpg\&crop\=entropy\&cs\=srgb\&dl\=pathum-danthanarayana-KLbUohEjb04-unsplash.jpg
+wget -A jpg https://github.com/makccr/wallpapers/blob/master/wallpapers/animals/NOUNS-hairy-penguins-2.jpg
+wget -A jpg https://images.unsplash.com/photo-1518599904199-0ca897819ddb?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=ameenfahmy-mXpTl4jNKiA-unsplash.jpg
+wget -A jpg https://github.com/makccr/wallpapers/blob/master/wallpapers/landscape/GENERAL-black-boat-sea.jpg
+wget -A jpg https://images.unsplash.com/photo-1455325528055-ad815afecebe?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=pineapple-supply-co-IQ0jqC0Jjuk-unsplash.jpg
+wget -A jpg https://github.com/makccr/wallpapers/blob/master/wallpapers/landscape/GENERAL-boats-on-the-sea.jpg
+wget -A jpg https://github.com/makccr/wallpapers/blob/master/wallpapers/landscape/NOUNS-ship-two.jpg
+wget -A jpg https://github.com/makccr/wallpapers/blob/master/wallpapers/landscape/PLACES-canary-islands.jpg

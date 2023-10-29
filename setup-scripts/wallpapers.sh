c2b5f50c1de17b3c48f0bba0f14a8e110348a0fa
jimbro <jimcaine.dev@gmail.com>
2023-10-29 15:53:43 -0500

    update wallpaper links

--- setup-scripts/wallpapers.sh
+++ setup-scripts/wallpapers.sh
@@ -1,11 +1,12 @@
 mkdir -p ~/.wallpapers
 cd ~/.wallpapers
+
 wget -A jpg https://github.com/makccr/wallpapers/raw/master/wallpapers/animals/anna-wangler-_GqwoiT7QY8-unsplash.jpg
-wget -A jpg https://images.unsplash.com/photo-1534870439272-475575042b61\?ixlib\=rb-4.0.3\&q\=85\&fm\=jpg\&crop\=entropy\&cs\=srgb\&dl\=pathum-danthanarayana-KLbUohEjb04-unsplash.jpg
-wget -A jpg https://github.com/makccr/wallpapers/blob/master/wallpapers/animals/NOUNS-hairy-penguins-2.jpg
-wget -A jpg https://images.unsplash.com/photo-1518599904199-0ca897819ddb?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=ameenfahmy-mXpTl4jNKiA-unsplash.jpg
 wget -A jpg https://github.com/makccr/wallpapers/blob/master/wallpapers/landscape/GENERAL-black-boat-sea.jpg
+
+wget -A jpg https://images.unsplash.com/photo-1534870439272-475575042b61\?ixlib\=rb-4.0.3\&q\=85\&fm\=jpg\&crop\=entropy\&cs\=srgb\&dl\=pathum-danthanarayana-KLbUohEjb04-unsplash.jpg
+wget -A jpg https://images.unsplash.com/photo-1518599904199-0ca897819ddb?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=ameenfahmy-mXpTl4jNKiA-unsplash.jpg
 wget -A jpg https://images.unsplash.com/photo-1455325528055-ad815afecebe?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=pineapple-supply-co-IQ0jqC0Jjuk-unsplash.jpg
-wget -A jpg https://github.com/makccr/wallpapers/blob/master/wallpapers/landscape/GENERAL-boats-on-the-sea.jpg
-wget -A jpg https://github.com/makccr/wallpapers/blob/master/wallpapers/landscape/NOUNS-ship-two.jpg
-wget -A jpg https://github.com/makccr/wallpapers/blob/master/wallpapers/landscape/PLACES-canary-islands.jpg
+
+wget https://h2.gifposter.com/bingImages/WinterBison_EN-US1438810655_1920x1080.jpg
+wget https://h2.gifposter.com/bingImages/FaceOff_EN-US7097284176_1920x1080.jpg

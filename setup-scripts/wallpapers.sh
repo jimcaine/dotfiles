991021a70f3e825ae431dc284bf1fbe469f3e943
jimbro <jimcaine.dev@gmail.com>
2024-06-05 22:42:38 -0500

    add wallpapers

--- setup-scripts/wallpapers.sh
+++ setup-scripts/wallpapers.sh
@@ -10,3 +10,11 @@ wget -A jpg https://images.unsplash.com/photo-1455325528055-ad815afecebe?ixlib=r
 
 wget https://h2.gifposter.com/bingImages/WinterBison_EN-US1438810655_1920x1080.jpg
 wget https://h2.gifposter.com/bingImages/FaceOff_EN-US7097284176_1920x1080.jpg
+wget https://imgproxy.nanxiongnandi.com/k7NVE-a-ywgbgOvqETO8B0CHIpGwHeiLibcnrXgjI5Y/w:3840/q:100/att:1/aHR0cHM6Ly9pbWcu/bmFueGlvbmduYW5k/aS5jb20vMjAyMzEx/L0JhZGxhbmRzU3Vu/cmlzZS5qcGc.jpg
+wget https://imgproxy.nanxiongnandi.com/0Sv4FemT0MR5r6V5HOe9gGDnQkCHK-LUJdChUpFn8c8/w:3840/q:100/att:1/aHR0cHM6Ly9pbWcu/bmFueGlvbmduYW5k/aS5jb20vMjAyMzEx/L0Jpc29uU25vdy5q/cGc.jpg
+wget https://imgproxy.nanxiongnandi.com/JdJg8e5d_0hzuw0P2sp3lVz9cDNsWDxXVlHi6wumBXc/w:3840/q:100/att:1/aHR0cHM6Ly9pbWcu/bmFueGlvbmduYW5k/aS5jb20vMjAyMzEw/L0FzdG9yaWFCcmlk/Z2UuanBn.jpg
+wget https://imgproxy.nanxiongnandi.com/-n8vTpEyotyjxl0KhEdMOIAV-ga4q55aiP95Ybxk5kQ/w:3840/q:100/att:1/aHR0cHM6Ly9pbWcu/bmFueGlvbmduYW5k/aS5jb20vMjAyMzEw/L1ZpZXN0ZUl0YWx5/LmpwZw.jpg
+wget https://imgproxy.nanxiongnandi.com/RIWcChkRnFWjITkHkSvkPGguacUuuPfs37hfVaU4CzQ/w:3840/q:100/att:1/aHR0cHM6Ly9pbWcu/bmFueGlvbmduYW5k/aS5jb20vMjAyMzEw/L1dhdGVyTGlseVZp/ZXRuYW0uanBn.jpg
+wget https://imgproxy.nanxiongnandi.com/HfeMQUWw6Yefaid3ORpDOBRt9nkL60LtLk8Q0wtaCmk/w:3840/q:100/att:1/aHR0cHM6Ly9pbWcu/bmFueGlvbmduYW5k/aS5jb20vMjAyMzEw/L0tvZGlha0FsYXNr/YS5qcGc.jpg
+wget https://imgproxy.nanxiongnandi.com/WYJYZaecqZX1k0wC4wp_Gl_eZxyui5UcltSUM2LMZXU/w:3840/q:100/att:1/aHR0cHM6Ly9pbWcu/bmFueGlvbmduYW5k/aS5jb20vMjAyMzEw/L0dlbnRvb0p1bXAu/anBn.jpg
+wget https://imgproxy.nanxiongnandi.com/gEgJUgd3s9PxlVR-DcaInu94HXG-72ne3_LgSSgy-fc/w:3840/q:100/att:1/aHR0cHM6Ly9pbWcu/bmFueGlvbmduYW5k/aS5jb20vMjAyMzEw/L0dyaXp6bHlGYWxs/cy5qcGc.jpg

11246786e0b281dee14ec272e8e608b2891ced27
jimbro <jimcaine.dev@gmail.com>
2024-08-05 19:23:04 -0500

    feature: add wallpapers

--- setup/common/_wallpapers-makeccr.sh
+++ setup/common/_wallpapers-makeccr.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 mkdir -p ~/.local/wallpaper/makeccr
-git clone https://github.com/makccr/wallpapers.git
-mv wallpapers/wallpapers/* ~/.local/wallpaper/makeccr/
-rm -rf wallpapers
 
-echo "Linking wallpapers..."
-for file in ~/.local/wallpaper/makeccr/*/*; do
-  ln -s $file ~/.local/wallpaper/$(basename $file)
-done
+cd ~/.local/wallpaper/makeccr
 
-# delete linked wallpapers:
-# find ~/.local/wallpaper/ -type l -delete
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/animals/anna-wangler-_GqwoiT7QY8-unsplash.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/animals/karl-ibri-CQqZ2BWeoAU-unsplash.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/landscape/GENERAL-black-boat-sea.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/landscape/NOUNS-ship-two.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/landscape/PLACES-canary-islands.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/landscape/PLACES-yacht.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/landscape/alex-perez-marco-island.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/landscape/alex-perez-wooden-pier-marco-island.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/landscape/johny-vino-new-york-harbour.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/landscape/shifaaz-shamoon-Rl9l9mL6Pvs-unsplash.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/light/GENERAL-ipad-gold.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/light/GENERAL-ipad-rose-gold.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/light/jon-flobrant-YD1uvthZwg4-unsplash.jpg
+curl -O https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/light/sylwia-bartyzel-tME8s001BNQ-unsplash.jpg
+
+cd -

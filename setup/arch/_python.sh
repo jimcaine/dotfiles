d4781a86b247fc8c48fb57b8166385f33f55860a
jimcaine <jimcaine.dev@gmail.com>
2024-10-19 16:45:55 +0000

    feature: setup pyenv

--- setup/arch/_python.sh
+++ setup/arch/_python.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 sudo curl https://pyenv.run | bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"

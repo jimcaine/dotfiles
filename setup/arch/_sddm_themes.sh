6aa257667ce3bf7753784b405bdc2548e4942468
Jim Caine <jimcaine.dev@gmail.com>
2024-11-13 21:22:41 -0600

    sddm: add sugar-candy.conf theme

--- setup/arch/_sddm_themes.sh
+++ setup/arch/_sddm_themes.sh
@@ -2,4 +2,4 @@
 yay -Sy --noconfirm sddm-theme-sugar-candy-git sddm-theme-greenleaf
 sudo cp ~/.dotfiles/dotfiles/sddm/sddm.conf /etc/sddm.conf
 sudo cp ~/.local/share/wallpapers/WinterBison.jpg /usr/share/sddm/themes/Sugar-Candy/Backgrounds/WinterBison.jpg
-sudo sed -i 's|Backgrounds/Mountain.jpg|Backgrounds/WinterBison.jpg|g' /usr/share/sddm/themes/Sugar-Candy/theme.conf
+sudo cp ~/.dotfiles/dotfiles/sddm/themes/sugar-candy.conf /usr/share/sddm/themes/Sugar-Candy/theme.conf

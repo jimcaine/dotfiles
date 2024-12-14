336a085d02eb9d69345db036149c155a6bef3eff
Jim Caine <jimcaine.dev@gmail.com>
2024-12-14 14:08:58 -0600

    firefox: add wayland profile

---
+++ dotfiles/firefox/wayland.js
@@ -0,0 +1,5 @@
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.rdd-ffmpeg.enabled", true);
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.enabled", true);
+user_pref("widget.wayland-dmabuf-vaapi.enabled", true);

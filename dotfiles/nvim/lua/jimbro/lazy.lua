3ec31b459cbf86f17e2c94b814cb62dd574c9d60
jimcaine <caine.jim@gmail.com>
2026-05-03 14:50:46 -0500

    nvim: update lazy.lua to setup each plugin individually (better troubleshooting)

--- dotfiles/nvim/lua/jimbro/lazy.lua
+++ dotfiles/nvim/lua/jimbro/lazy.lua
@@ -12,6 +12,23 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { import = "jimbro.plugins" },
+  { import = "jimbro.plugins.colorizer" },
+  { import = "jimbro.plugins.colors" },
+  { import = "jimbro.plugins.commentary" },
+  { import = "jimbro.plugins.copilot" },
+  { import = "jimbro.plugins.dressing" },
+  { import = "jimbro.plugins.harpoon" },
   { import = "jimbro.plugins.lsp" },
+  { import = "jimbro.plugins.lualine" },
+  { import = "jimbro.plugins.markdown" },
+  { import = "jimbro.plugins.noice" },
+  { import = "jimbro.plugins.notify" },
+  { import = "jimbro.plugins.nvim-cmp" },
+  { import = "jimbro.plugins.nvim-tree" },
+  { import = "jimbro.plugins.pencil" },
+  { import = "jimbro.plugins.startify" },
+  { import = "jimbro.plugins.telescope" },
+  { import = "jimbro.plugins.treesitter" },
+  { import = "jimbro.plugins.twilight" },
+  { import = "jimbro.plugins.zen" },
 })

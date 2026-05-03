48f831b50986da28db2f509c3c1d771366376a29
jimcaine <caine.jim@gmail.com>
2026-05-03 14:58:27 -0500

    nvim: disable treesitter in the Telescope previewer defaults

--- dotfiles/nvim/lua/jimbro/plugins/telescope.lua
+++ dotfiles/nvim/lua/jimbro/plugins/telescope.lua
@@ -1,8 +1,14 @@
 return {
   "nvim-telescope/telescope.nvim",
   lazy = false,
-  config = true,
   dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    defaults = {
+      preview = {
+        treesitter = false,
+      },
+    },
+  },
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
